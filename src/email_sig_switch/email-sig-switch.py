d607787d0c345ff16a9baaa59d0ee2eb0ca2f2b5
Zan Owsley <zanowsley@gmail.com>
2025-01-09 20:55:41 -0700

    Got basic thing working

---
+++ src/email_sig_switch/email-sig-switch.py
@@ -0,0 +1,12 @@
+from zan_dmenu import select_file_or_dir
+from shutil import copyfile
+
+
+PROMPT = "Which signature do you want?"
+SIGS_BASE_DIR = "/home/zan/sync/dat/email-signatures/"
+SIGS_DIR = SIGS_BASE_DIR + "saved-signatures/"
+SIG_FILE = SIGS_BASE_DIR + "signature.html"
+
+def switch_signature() -> None:
+    new_sig_file = select_file_or_dir(PROMPT, SIGS_DIR, recurse=True)
+    copyfile(new_sig_file, SIG_FILE)
