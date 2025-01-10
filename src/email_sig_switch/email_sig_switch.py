7ef7f2c765387554875e3d790dece9e890cf8718
Zan Owsley <zanowsley@gmail.com>
2025-01-09 21:16:27 -0700

    Fixed it up and it's working and installable

--- src/email_sig_switch/email_sig_switch.py
+++ src/email_sig_switch/email_sig_switch.py
@@ -1,4 +1,4 @@
-from zan_dmenu import select_file_or_dir
+from email_sig_switch.zan_dmenu import select_file_or_dir
 from shutil import copyfile
 
 
@@ -7,6 +7,6 @@ SIGS_BASE_DIR = "/home/zan/sync/dat/email-signatures/"
 SIGS_DIR = SIGS_BASE_DIR + "saved-signatures/"
 SIG_FILE = SIGS_BASE_DIR + "signature.html"
 
-def switch_signature() -> None:
+def switch_signature():
     new_sig_file = select_file_or_dir(PROMPT, SIGS_DIR, recurse=True)
     copyfile(new_sig_file, SIG_FILE)
